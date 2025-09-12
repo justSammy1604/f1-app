1b71f16eca33b085c1c5d9551ea6a8ee53da5c4c
Samuel Mesquita <sammesquita1604@gmail.com>
2025-09-12 06:20:13 +0530

    asfg

--- src/components/ui/Skeleton.jsx
+++ src/components/ui/Skeleton.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export function Skeleton({ className = '' }) {
+export function Skeleton({ className = '' }) { 
   return <div className={`animate-pulse rounded-md bg-neutral-200 dark:bg-neutral-800 ${className}`} />
 }
